6f9dd606fa91d97f556b3fdd89f2f2ea4f8ccae7
Mani Khamp <manidone@yahoo.com>
2023-03-03 10:58:38 -0800

    End of section 16

--- API/Dtos/AddressDto.cs
+++ API/Dtos/AddressDto.cs
@@ -1,12 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dtos
 {
     public class AddressDto
     {
+        [Required]
         public string Firstname { get; set; }
+        [Required]
         public string Lastname { get; set; }
+        [Required]
         public string Street { get; set; }
+        [Required]
         public string City { get; set; }
+        [Required]
         public string State { get; set; }
+        [Required]
         public string ZipCode { get; set; }
     }
 }
